0d2598ce9bf08407789779a92f0205f2e1e75785
celicoo <marcelo.franco.alves@gmail.com>
2020-03-19 16:59:05 -0300

    chore(types): add interfaces AbstractedDatabase, ClientDatabase, AdminConfig and ClientConfig

---
+++ packages/types/index.d.ts
@@ -0,0 +1,46 @@
+import { AppOptions } from 'firebase-admin';
+import { FirebaseOptions } from '@firebase/app-types';
+
+/**
+ * The interface that abstract databases must implement.
+ */
+export interface AbstractedDatabase {
+  /**
+   * Connects to the database.
+   */
+  connect(): void;
+}
+
+/**
+ * The interface that client databases must implement.
+ */
+export interface ClientDatabase {}
+
+/**
+ * Available admin configurations.
+ */
+export interface AdminConfig extends AppOptions {
+  /**
+   * The ID of the service account to be used for signing custom tokens. This
+   * can be found in the `client_email` field of a service account JSON file.
+   */
+  serviceAccountId: string;
+  /**
+   * The ID of the Google Cloud project associated with the App.
+   */
+  projectId: string;
+}
+
+/**
+ * Available client configurations.
+ */
+export interface ClientConfig extends FirebaseOptions {
+  /**
+   * The URL of the database from which to read and write data.
+   */
+  databaseURL: string;
+  /**
+   * The ID of the Google Cloud project associated with the App.
+   */
+  projectId: string;
+}
