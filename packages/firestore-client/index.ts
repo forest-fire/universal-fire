f8dd3be68cdff7ee11cc45cafce4e17b449cbe75
celicoo <marcelo.franco.alves@gmail.com>
2020-03-19 17:07:06 -0300

    feat(firestore-client): add FirestoreClient class

--- packages/firestore-client/index.ts
+++ packages/firestore-client/index.ts
@@ -1,1 +1,10 @@
+import { ClientConfig, AbstractedDatabase } from '@forest-fire/types';
+import { FirestoreDb } from '@forest-fire/firestore';
 
+export class FirestoreClient extends FirestoreDb implements AbstractedDatabase {
+  static connect(config: ClientConfig) {
+    const db = new FirestoreClient(config);
+    db.connect();
+    return db;
+  }
+}
