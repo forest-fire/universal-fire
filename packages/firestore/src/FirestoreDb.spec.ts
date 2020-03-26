fd5a9373bd58bb27efd206382b63e3e790e7591f
celicoo <marcelo.franco.alves@gmail.com>
2020-03-26 17:13:04 -0300

    feat(firestore): - add scripts to build and test package - add tests - add tsconfig - restructure package

---
+++ packages/firestore/src/FirestoreDb.spec.ts
@@ -0,0 +1,7 @@
+import { FirestoreDb } from './FirestoreDb';
+
+describe('FirestoreDb', () => {
+  it('should be defined', () => {
+    expect(FirestoreDb).toBeDefined();
+  });
+});
--- packages/firestore/src/FirestoreDb.ts
+++ packages/firestore/src/FirestoreDb.ts
@@ -2,9 +2,8 @@ import '@firebase/firestore';
 import { ClientDatabase } from '@forest-fire/types';
 import { Database } from '@forest-fire/database';
 
-export class FirestoreDb extends Database implements ClientDatabase {
+export abstract class FirestoreDb extends Database implements ClientDatabase {
   public connect() {
-    this.initializeApp();
-    this.database = this.app.firestore();
+    this.database = this.app.firestore!();
   }
 }
