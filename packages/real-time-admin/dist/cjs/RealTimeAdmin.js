2c8ffbd5ad07a2550f024f5514a5d54acc85abd3
celicoo <marcelo.franco.alves@gmail.com>
2020-05-18 15:29:02 -0300

    chore(real-ime-admin): fix connection

--- packages/real-time-admin/dist/cjs/RealTimeAdmin.js
+++ packages/real-time-admin/dist/cjs/RealTimeAdmin.js
@@ -29,145 +29,156 @@ const types_1 = require("@forest-fire/types");
 const utility_1 = require("@forest-fire/utility");
 const RealTimeAdminError_1 = require("./errors/RealTimeAdminError");
 const firemock_1 = require("firemock");
-class RealTimeAdmin extends real_time_db_1.RealTimeDb {
-    constructor(config) {
-        super();
-        this._isAdminApi = true;
-        this._clientType = 'admin';
-        this._isAuthorized = true;
-        this._eventManager = new EventManager_1.EventManager();
-        this.CONNECTION_TIMEOUT = config ? config.timeout || 5000 : 5000;
-        if (!config) {
-            config = {
-                serviceAccount: utility_1.extractServiceAccount(config),
-                databaseURL: utility_1.extractDataUrl(config),
-            };
-        }
-        if (types_1.isAdminConfig(config)) {
-            config.serviceAccount =
-                config.serviceAccount || utility_1.extractServiceAccount(config);
-            config.databaseURL = config.databaseURL || utility_1.extractDataUrl(config);
-            config.name = utility_1.determineDefaultAppName(config);
-            this._config = config;
-            const runningApps = utility_1.getRunningApps(firebase.apps);
-            const credential = firebase.credential.cert(config.serviceAccount);
-            this.app = runningApps.includes(config.name)
-                ? utility_1.getRunningFirebaseApp(config.name, firebase.apps)
-                : firebase.initializeApp({
-                    credential,
-                    databaseURL: config.databaseURL,
-                }, config.name);
-        }
-        else if (types_1.isMockConfig(config)) {
-            config.name = utility_1.determineDefaultAppName(config);
-            this._config = config;
-        }
-        else {
-            throw new utility_1.FireError(`The configuration sent into an Admin SDK abstraction was invalid and may be a client SDK configuration instead. The configuration was: \n${JSON.stringify(config, null, 2)}`, 'invalid-configuration');
-        }
-    }
-    /**
-     * Instantiates a DB and then waits for the connection
-     * to finish before resolving the promise.
-     */
-    static async connect(config) {
-        const obj = new RealTimeAdmin(config);
-        await obj.connect();
-        return obj;
-    }
-    get app() {
-        if (this._app) {
-            return this._app;
-        }
-        throw new utility_1.FireError('Attempt to access Firebase App without having instantiated it');
-    }
-    set app(value) {
-        this._app = value;
-    }
-    /**
-     * Provides access to the Firebase Admin Auth API.
-     *
-     * > If using a _mocked_ database then the Auth API will be redirected to **firemock**
-     * instead of the real Admin SDK for Auth. Be aware that this mocked API may not be fully implemented
-     * but PR's are welcome if the part you need is not yet implemented. If you want to explicitly state
-     * whether to use the _real_ or _mock_ Auth SDK then you can state this by passing in a `auth` parameter
-     * as part of the configuration (using either "real" or "mocked" as a value)
-     *
-     * References:
-     * - [Introduction](https://firebase.google.com/docs/auth/admin)
-     * - [API](https://firebase.google.com/docs/reference/admin/node/admin.auth.Auth)
-     */
-    async auth() {
-        if (this._config.mocking) {
-            return firemock_1.adminAuthSdk;
-        }
-        return firebase.auth(this._app);
-    }
-    goOnline() {
-        if (this._database) {
-            try {
-                this._database.goOnline();
+let RealTimeAdmin = /** @class */ (() => {
+    class RealTimeAdmin extends real_time_db_1.RealTimeDb {
+        constructor(config) {
+            super();
+            this._clientType = "admin";
+            this._isAuthorized = true;
+            this._isAdminApi = true;
+            this._eventManager = new EventManager_1.EventManager();
+            this.CONNECTION_TIMEOUT = config ? config.timeout || 5000 : 5000;
+            if (!config) {
+                config = {
+                    serviceAccount: utility_1.extractServiceAccount(config),
+                    databaseURL: utility_1.extractDataUrl(config),
+                };
             }
-            catch (e) {
-                util_1.debug('There was an error going online:' + e);
+            if (types_1.isAdminConfig(config)) {
+                config.serviceAccount = config.serviceAccount || utility_1.extractServiceAccount(config);
+                config.databaseURL = config.databaseURL || utility_1.extractDataUrl(config);
+                config.name = utility_1.determineDefaultAppName(config);
+                this._config = config;
+                const runningApps = utility_1.getRunningApps(firebase.apps);
+                const credential = firebase.credential.cert(config.serviceAccount);
+                this.app = runningApps.includes(config.name)
+                    ? utility_1.getRunningFirebaseApp(config.name, firebase.apps)
+                    : firebase.initializeApp({
+                        credential,
+                        databaseURL: config.databaseURL,
+                    }, config.name);
+            }
+            else if (types_1.isMockConfig(config)) {
+                config.name = utility_1.determineDefaultAppName(config);
+                this._config = config;
+            }
+            else {
+                throw new utility_1.FireError(`The configuration sent into an Admin SDK abstraction was invalid and may be a client SDK configuration instead. The configuration was: \n${JSON.stringify(config, null, 2)}`, "invalid-configuration");
             }
         }
-        else {
-            console.warn('Attempt to use goOnline() prior to having a database connection!');
+        /**
+         * Instantiates a DB and then waits for the connection
+         * to finish before resolving the promise.
+         */
+        static async connect(config) {
+            const obj = new RealTimeAdmin(config);
+            await obj.connect();
+            return obj;
         }
-    }
-    goOffline() {
-        if (this._database) {
-            this._database.goOffline();
+        static get connections() {
+            return RealTimeAdmin._connections;
         }
-        else {
-            console.warn('Attempt to use goOffline() prior to having a database connection!');
+        static addConnection(app) {
+            if (RealTimeAdmin._connections[app.name]) {
+                throw new RealTimeAdminError_1.RealTimeAdminError(`Attempt to add app with name that already exists! [${app.name}]`, "not-allowed");
+            }
+            RealTimeAdmin._connections[app.name] = app;
         }
-    }
-    async connect() {
-        if (types_1.isMockConfig(this._config)) {
-            await this._connectMockDb(this._config);
+        get app() {
+            if (this._app) {
+                return this._app;
+            }
+            throw new utility_1.FireError("Attempt to access Firebase App without having instantiated it");
         }
-        else if (types_1.isAdminConfig(this._config)) {
-            await this._connectRealDb(this._config);
+        set app(value) {
+            this._app = value;
         }
-        else {
-            throw new RealTimeAdminError_1.RealTimeAdminError('The configuation passed is not valid for an admin SDK!', 'invalid-configuration');
+        /**
+         * Provides access to the Firebase Admin Auth API.
+         *
+         * > If using a _mocked_ database then the Auth API will be redirected to **firemock**
+         * instead of the real Admin SDK for Auth. Be aware that this mocked API may not be fully implemented
+         * but PR's are welcome if the part you need is not yet implemented. If you want to explicitly state
+         * whether to use the _real_ or _mock_ Auth SDK then you can state this by passing in a `auth` parameter
+         * as part of the configuration (using either "real" or "mocked" as a value)
+         *
+         * References:
+         * - [Introduction](https://firebase.google.com/docs/auth/admin)
+         * - [API](https://firebase.google.com/docs/reference/admin/node/admin.auth.Auth)
+         */
+        async auth() {
+            if (this._config.mocking) {
+                return firemock_1.adminAuthSdk;
+            }
+            return firebase.auth(this._app);
         }
-        return this;
-    }
-    async _connectMockDb(config) {
-        await this.getFireMock({
-            db: config.mockData || {},
-            auth: { providers: [], ...config.mockAuth },
-        });
-        this._isConnected = true;
-        return this;
-    }
-    async _connectRealDb(config) {
-        if (!this._isConnected) {
+        goOnline() {
+            if (this._database) {
+                try {
+                    this._database.goOnline();
+                }
+                catch (e) {
+                    util_1.debug("There was an error going online:" + e);
+                }
+            }
+            else {
+                console.warn("Attempt to use goOnline() prior to having a database connection!");
+            }
+        }
+        goOffline() {
+            if (this._database) {
+                this._database.goOffline();
+            }
+            else {
+                console.warn("Attempt to use goOffline() prior to having a database connection!");
+            }
+        }
+        get isConnected() {
+            return (this.app && this.config?.name && utility_1.getRunningApps(firebase.apps).includes(this.config.name));
+        }
+        async connect() {
+            if (types_1.isMockConfig(this._config)) {
+                await this._connectMockDb(this._config);
+            }
+            else if (types_1.isAdminConfig(this._config)) {
+                await this._connectRealDb(this._config);
+            }
+            else {
+                throw new RealTimeAdminError_1.RealTimeAdminError("The configuation passed is not valid for an admin SDK!", "invalid-configuration");
+            }
+            return this;
+        }
+        async _connectMockDb(config) {
+            await this.getFireMock({
+                db: config.mockData || {},
+                auth: { providers: [], ...config.mockAuth },
+            });
+            return this;
+        }
+        async _connectRealDb(config) {
             this._database = this._app.database();
             this.enableDatabaseLogging = firebase.database.enableLogging.bind(firebase.database);
             this.goOnline();
             this._eventManager.connection(true);
             await this._listenForConnectionStatus();
+            if (this.isConnected) {
+                console.info(`Database ${this.app.name} was already connected. Reusing connection.`);
+            }
         }
-        else {
-            console.info(`Database ${config.name} already connected`);
+        /**
+         * listenForConnectionStatus
+         *
+         * in the admin interface we assume that ONCE connected
+         * we remain connected; this is unlike the client API
+         * which provides an endpoint to lookup
+         */
+        async _listenForConnectionStatus() {
+            this._setupConnectionListener();
+            this._eventManager.connection(true);
         }
     }
-    /**
-     * listenForConnectionStatus
-     *
-     * in the admin interface we assume that ONCE connected
-     * we remain connected; this is unlike the client API
-     * which provides an endpoint to lookup
-     */
-    async _listenForConnectionStatus() {
-        this._setupConnectionListener();
-        this._isConnected = true;
-        this._eventManager.connection(true);
-    }
-}
+    RealTimeAdmin._connections = {};
+    return RealTimeAdmin;
+})();
 exports.RealTimeAdmin = RealTimeAdmin;
 //# sourceMappingURL=RealTimeAdmin.js.map
--- packages/real-time-admin/dist/es/RealTimeAdmin.js
+++ packages/real-time-admin/dist/es/RealTimeAdmin.js
@@ -1,150 +1,162 @@
 // TODO: reduce this to just named symbols which we need!
-import * as firebase from 'firebase-admin';
-import { RealTimeDb } from '@forest-fire/real-time-db';
-import { EventManager } from './EventManager';
-import { debug } from './util';
-import { isMockConfig, isAdminConfig, } from '@forest-fire/types';
-import { extractServiceAccount, FireError, getRunningApps, extractDataUrl, getRunningFirebaseApp, determineDefaultAppName, } from '@forest-fire/utility';
-import { RealTimeAdminError } from './errors/RealTimeAdminError';
-import { adminAuthSdk } from 'firemock';
-export class RealTimeAdmin extends RealTimeDb {
-    constructor(config) {
-        super();
-        this._isAdminApi = true;
-        this._clientType = 'admin';
-        this._isAuthorized = true;
-        this._eventManager = new EventManager();
-        this.CONNECTION_TIMEOUT = config ? config.timeout || 5000 : 5000;
-        if (!config) {
-            config = {
-                serviceAccount: extractServiceAccount(config),
-                databaseURL: extractDataUrl(config),
-            };
-        }
-        if (isAdminConfig(config)) {
-            config.serviceAccount =
-                config.serviceAccount || extractServiceAccount(config);
-            config.databaseURL = config.databaseURL || extractDataUrl(config);
-            config.name = determineDefaultAppName(config);
-            this._config = config;
-            const runningApps = getRunningApps(firebase.apps);
-            const credential = firebase.credential.cert(config.serviceAccount);
-            this.app = runningApps.includes(config.name)
-                ? getRunningFirebaseApp(config.name, firebase.apps)
-                : firebase.initializeApp({
-                    credential,
-                    databaseURL: config.databaseURL,
-                }, config.name);
-        }
-        else if (isMockConfig(config)) {
-            config.name = determineDefaultAppName(config);
-            this._config = config;
-        }
-        else {
-            throw new FireError(`The configuration sent into an Admin SDK abstraction was invalid and may be a client SDK configuration instead. The configuration was: \n${JSON.stringify(config, null, 2)}`, 'invalid-configuration');
-        }
-    }
-    /**
-     * Instantiates a DB and then waits for the connection
-     * to finish before resolving the promise.
-     */
-    static async connect(config) {
-        const obj = new RealTimeAdmin(config);
-        await obj.connect();
-        return obj;
-    }
-    get app() {
-        if (this._app) {
-            return this._app;
-        }
-        throw new FireError('Attempt to access Firebase App without having instantiated it');
-    }
-    set app(value) {
-        this._app = value;
-    }
-    /**
-     * Provides access to the Firebase Admin Auth API.
-     *
-     * > If using a _mocked_ database then the Auth API will be redirected to **firemock**
-     * instead of the real Admin SDK for Auth. Be aware that this mocked API may not be fully implemented
-     * but PR's are welcome if the part you need is not yet implemented. If you want to explicitly state
-     * whether to use the _real_ or _mock_ Auth SDK then you can state this by passing in a `auth` parameter
-     * as part of the configuration (using either "real" or "mocked" as a value)
-     *
-     * References:
-     * - [Introduction](https://firebase.google.com/docs/auth/admin)
-     * - [API](https://firebase.google.com/docs/reference/admin/node/admin.auth.Auth)
-     */
-    async auth() {
-        if (this._config.mocking) {
-            return adminAuthSdk;
-        }
-        return firebase.auth(this._app);
-    }
-    goOnline() {
-        if (this._database) {
-            try {
-                this._database.goOnline();
+import * as firebase from "firebase-admin";
+import { RealTimeDb } from "@forest-fire/real-time-db";
+import { EventManager } from "./EventManager";
+import { debug } from "./util";
+import { isMockConfig, isAdminConfig, } from "@forest-fire/types";
+import { extractServiceAccount, FireError, getRunningApps, extractDataUrl, getRunningFirebaseApp, determineDefaultAppName, } from "@forest-fire/utility";
+import { RealTimeAdminError } from "./errors/RealTimeAdminError";
+import { adminAuthSdk } from "firemock";
+let RealTimeAdmin = /** @class */ (() => {
+    class RealTimeAdmin extends RealTimeDb {
+        constructor(config) {
+            super();
+            this._clientType = "admin";
+            this._isAuthorized = true;
+            this._isAdminApi = true;
+            this._eventManager = new EventManager();
+            this.CONNECTION_TIMEOUT = config ? config.timeout || 5000 : 5000;
+            if (!config) {
+                config = {
+                    serviceAccount: extractServiceAccount(config),
+                    databaseURL: extractDataUrl(config),
+                };
             }
-            catch (e) {
-                debug('There was an error going online:' + e);
+            if (isAdminConfig(config)) {
+                config.serviceAccount = config.serviceAccount || extractServiceAccount(config);
+                config.databaseURL = config.databaseURL || extractDataUrl(config);
+                config.name = determineDefaultAppName(config);
+                this._config = config;
+                const runningApps = getRunningApps(firebase.apps);
+                const credential = firebase.credential.cert(config.serviceAccount);
+                this.app = runningApps.includes(config.name)
+                    ? getRunningFirebaseApp(config.name, firebase.apps)
+                    : firebase.initializeApp({
+                        credential,
+                        databaseURL: config.databaseURL,
+                    }, config.name);
+            }
+            else if (isMockConfig(config)) {
+                config.name = determineDefaultAppName(config);
+                this._config = config;
+            }
+            else {
+                throw new FireError(`The configuration sent into an Admin SDK abstraction was invalid and may be a client SDK configuration instead. The configuration was: \n${JSON.stringify(config, null, 2)}`, "invalid-configuration");
             }
         }
-        else {
-            console.warn('Attempt to use goOnline() prior to having a database connection!');
+        /**
+         * Instantiates a DB and then waits for the connection
+         * to finish before resolving the promise.
+         */
+        static async connect(config) {
+            const obj = new RealTimeAdmin(config);
+            await obj.connect();
+            return obj;
         }
-    }
-    goOffline() {
-        if (this._database) {
-            this._database.goOffline();
+        static get connections() {
+            return RealTimeAdmin._connections;
         }
-        else {
-            console.warn('Attempt to use goOffline() prior to having a database connection!');
+        static addConnection(app) {
+            if (RealTimeAdmin._connections[app.name]) {
+                throw new RealTimeAdminError(`Attempt to add app with name that already exists! [${app.name}]`, "not-allowed");
+            }
+            RealTimeAdmin._connections[app.name] = app;
         }
-    }
-    async connect() {
-        if (isMockConfig(this._config)) {
-            await this._connectMockDb(this._config);
+        get app() {
+            if (this._app) {
+                return this._app;
+            }
+            throw new FireError("Attempt to access Firebase App without having instantiated it");
         }
-        else if (isAdminConfig(this._config)) {
-            await this._connectRealDb(this._config);
+        set app(value) {
+            this._app = value;
         }
-        else {
-            throw new RealTimeAdminError('The configuation passed is not valid for an admin SDK!', 'invalid-configuration');
+        /**
+         * Provides access to the Firebase Admin Auth API.
+         *
+         * > If using a _mocked_ database then the Auth API will be redirected to **firemock**
+         * instead of the real Admin SDK for Auth. Be aware that this mocked API may not be fully implemented
+         * but PR's are welcome if the part you need is not yet implemented. If you want to explicitly state
+         * whether to use the _real_ or _mock_ Auth SDK then you can state this by passing in a `auth` parameter
+         * as part of the configuration (using either "real" or "mocked" as a value)
+         *
+         * References:
+         * - [Introduction](https://firebase.google.com/docs/auth/admin)
+         * - [API](https://firebase.google.com/docs/reference/admin/node/admin.auth.Auth)
+         */
+        async auth() {
+            if (this._config.mocking) {
+                return adminAuthSdk;
+            }
+            return firebase.auth(this._app);
         }
-        return this;
-    }
-    async _connectMockDb(config) {
-        await this.getFireMock({
-            db: config.mockData || {},
-            auth: { providers: [], ...config.mockAuth },
-        });
-        this._isConnected = true;
-        return this;
-    }
-    async _connectRealDb(config) {
-        if (!this._isConnected) {
+        goOnline() {
+            if (this._database) {
+                try {
+                    this._database.goOnline();
+                }
+                catch (e) {
+                    debug("There was an error going online:" + e);
+                }
+            }
+            else {
+                console.warn("Attempt to use goOnline() prior to having a database connection!");
+            }
+        }
+        goOffline() {
+            if (this._database) {
+                this._database.goOffline();
+            }
+            else {
+                console.warn("Attempt to use goOffline() prior to having a database connection!");
+            }
+        }
+        get isConnected() {
+            return (this.app && this.config?.name && getRunningApps(firebase.apps).includes(this.config.name));
+        }
+        async connect() {
+            if (isMockConfig(this._config)) {
+                await this._connectMockDb(this._config);
+            }
+            else if (isAdminConfig(this._config)) {
+                await this._connectRealDb(this._config);
+            }
+            else {
+                throw new RealTimeAdminError("The configuation passed is not valid for an admin SDK!", "invalid-configuration");
+            }
+            return this;
+        }
+        async _connectMockDb(config) {
+            await this.getFireMock({
+                db: config.mockData || {},
+                auth: { providers: [], ...config.mockAuth },
+            });
+            return this;
+        }
+        async _connectRealDb(config) {
             this._database = this._app.database();
             this.enableDatabaseLogging = firebase.database.enableLogging.bind(firebase.database);
             this.goOnline();
             this._eventManager.connection(true);
             await this._listenForConnectionStatus();
+            if (this.isConnected) {
+                console.info(`Database ${this.app.name} was already connected. Reusing connection.`);
+            }
         }
-        else {
-            console.info(`Database ${config.name} already connected`);
+        /**
+         * listenForConnectionStatus
+         *
+         * in the admin interface we assume that ONCE connected
+         * we remain connected; this is unlike the client API
+         * which provides an endpoint to lookup
+         */
+        async _listenForConnectionStatus() {
+            this._setupConnectionListener();
+            this._eventManager.connection(true);
         }
     }
-    /**
-     * listenForConnectionStatus
-     *
-     * in the admin interface we assume that ONCE connected
-     * we remain connected; this is unlike the client API
-     * which provides an endpoint to lookup
-     */
-    async _listenForConnectionStatus() {
-        this._setupConnectionListener();
-        this._isConnected = true;
-        this._eventManager.connection(true);
-    }
-}
+    RealTimeAdmin._connections = {};
+    return RealTimeAdmin;
+})();
+export { RealTimeAdmin };
 //# sourceMappingURL=RealTimeAdmin.js.map
