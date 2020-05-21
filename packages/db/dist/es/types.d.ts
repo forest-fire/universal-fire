4533180c0656dadfe9513a2be51234ea406278a0
Ken Snyder <ken@ken.net>
2020-05-21 11:28:53 -0700

    added interfaces for each of the SDK's

--- packages/db/dist/es/types.d.ts
+++ packages/db/dist/es/types.d.ts
@@ -2,21 +2,15 @@ import type { RealTimeAdmin } from '@forest-fire/real-time-admin';
 import type { RealTimeClient } from '@forest-fire/real-time-client';
 import type { FirestoreAdmin } from '@forest-fire/firestore-admin';
 import type { FirestoreClient } from '@forest-fire/firestore-client';
-
 /**
  * Provides a typing that includes all known supported SDK types
  */
-export type IAbstractedDatabase =
-  | RealTimeAdmin
-  | RealTimeClient
-  | FirestoreAdmin
-  | FirestoreClient;
-
+export declare type IAbstractedDatabase = RealTimeAdmin | RealTimeClient | FirestoreAdmin | FirestoreClient;
 /** The interface that the `RealTimeAdmin` class exposes */
-export type IRealTimeAdmin = RealTimeAdmin;
+export declare type IRealTimeAdmin = RealTimeAdmin;
 /** The interface that the `RealTimeClient` class exposes */
-export type IRealTimeClient = RealTimeClient;
+export declare type IRealTimeClient = RealTimeClient;
 /** The interface that the `FirestoreAdmin` class exposes */
-export type IFirestoreAdmin = FirestoreAdmin;
+export declare type IFirestoreAdmin = FirestoreAdmin;
 /** The interface that the `FirestoreClient` class exposes */
-export type IFirestoreClient = FirestoreClient;
+export declare type IFirestoreClient = FirestoreClient;
