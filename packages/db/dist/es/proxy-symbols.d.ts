9e19f40f147a474a386a65d4ed1382be3e89c45e
Ken Snyder <ken@ken.net>
2020-05-21 15:26:45 -0700

    universal-fire exports IClientAuth and IAdminAuth symbols

--- packages/db/dist/es/proxy-symbols.d.ts
+++ packages/db/dist/es/proxy-symbols.d.ts
@@ -1,2 +1,2 @@
-export { IAdminConfig, IMockConfig, IClientConfig, IRtdbDbEvent, IFirestoreDbEvent, } from '@forest-fire/types';
+export { IAdminConfig, IMockConfig, IClientConfig, IRtdbDbEvent, IFirestoreDbEvent, IClientAuth, IAdminAuth, } from '@forest-fire/types';
 export { SerializedQuery } from '@forest-fire/base-serializer';
--- packages/db/src/proxy-symbols.ts
+++ packages/db/src/proxy-symbols.ts
@@ -4,5 +4,7 @@ export {
   IClientConfig,
   IRtdbDbEvent,
   IFirestoreDbEvent,
+  IClientAuth,
+  IAdminAuth,
 } from '@forest-fire/types';
 export { SerializedQuery } from '@forest-fire/base-serializer';
