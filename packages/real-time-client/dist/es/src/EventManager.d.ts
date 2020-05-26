f9749dc3ec6f231b42a33d648a315bf657659da1
Ken Snyder <ken@ken.net>
2020-05-25 21:27:29 -0700

    fix (rtdb:client): make typing exposure better with IEmitter

--- packages/real-time-client/dist/es/src/EventManager.d.ts
+++ packages/real-time-client/dist/es/src/EventManager.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="node" />
-import * as events from 'events';
+import { EventEmitter } from 'events';
 import { IEmitter } from '@forest-fire/real-time-db';
 export { IEmitter };
-export declare class EventManager extends events.EventEmitter implements IEmitter {
+export declare class EventManager extends EventEmitter implements IEmitter {
     connection(state: boolean): void;
 }
