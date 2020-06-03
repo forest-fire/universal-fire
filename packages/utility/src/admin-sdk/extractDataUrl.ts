98ad8bf4a4f5f49e42f9f23d81cf629c5dcdfeea
Ken Snyder <ken@ken.net>
2020-06-02 22:17:06 -0700

    removes requirement for admin mocking to have a dataUrl

--- packages/utility/src/admin-sdk/extractDataUrl.ts
+++ packages/utility/src/admin-sdk/extractDataUrl.ts
@@ -6,15 +6,16 @@ import { FireError } from '../index';
  * configuration or via the FIREBASE_DATABASE_URL environment variable.
  */
 export function extractDataUrl(config?: IAdminConfig | IMockConfig) {
-  const dataUrl =
-    config && config.mocking !== true && config.databaseURL
-      ? config.databaseURL
-      : process.env['FIREBASE_DATABASE_URL'];
+  const dataUrl = config?.mocking
+    ? 'https://mocking.com'
+    : config.databaseURL
+    ? config.databaseURL
+    : process.env['FIREBASE_DATABASE_URL'];
   if (!dataUrl) {
     throw new FireError(
       `There was no DATABASE URL provided! This needs to be passed in as part of the configuration or as the FIREBASE_DATABASE_URL environment variable.`,
       'invalid-configuration'
     );
   }
-  return config && config.mocking ? 'https://mocking.com' : dataUrl;
+  return dataUrl;
 }
