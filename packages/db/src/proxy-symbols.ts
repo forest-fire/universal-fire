88c18869d76bc085aa30c8da872169dfd78f5a7b
Ken Snyder <ken@ken.net>
2020-06-17 11:49:10 -0700

    universal-fire now export Admin SDK's

--- packages/db/src/proxy-symbols.ts
+++ packages/db/src/proxy-symbols.ts
@@ -14,6 +14,8 @@ export {
 } from '@forest-fire/serialized-query';
 export type { RealTimeClient as IRealTimeClient } from '@forest-fire/real-time-client';
 export type { FirestoreClient as IFirestoreClient } from '@forest-fire/firestore-client';
+export type { RealTimeAdmin as IRealTimeAdmin } from '@forest-fire/real-time-admin';
+export type { FirestoreAdmin as IFirestoreAdmin } from '@forest-fire/firestore-admin';
 
 import { IRealTimeDb } from '@forest-fire/real-time-db';
 import { IFirestoreDb } from '@forest-fire/firestore-db';
