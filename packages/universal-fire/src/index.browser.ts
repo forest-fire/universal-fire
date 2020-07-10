e7f2d7b0cbbd9ce7eec3e878a12276b9a458fd4b
Ken Snyder <ken@ken.net>
2020-07-09 21:16:28 -0700

    chore: full comments and increased consistency in the SDK factories exported in universal-fire

--- packages/universal-fire/src/index.browser.ts
+++ packages/universal-fire/src/index.browser.ts
@@ -1,54 +1,113 @@
 export * from './proxy-symbols';
+export * from './sdk-types';
+import {
+  IFirestoreAdmin,
+  IFirestoreClient,
+  IRealTimeAdmin,
+  IRealTimeClient,
+} from './sdk-types';
 
-export type { RealTimeClient as IRealTimeClient } from '@forest-fire/real-time-client';
-export type { FirestoreClient as IFirestoreClient } from '@forest-fire/firestore-client';
-
-import type { RealTimeAdmin as IRealTimeAdmin } from '@forest-fire/real-time-admin';
-import type { FirestoreAdmin as IFirestoreAdmin } from '@forest-fire/firestore-admin';
 import { FirestoreClient as FC } from '@forest-fire/firestore-client';
 import { RealTimeClient as RTC } from '@forest-fire/real-time-client';
-import { IClientConfig, IMockConfig } from '@forest-fire/types';
+import { IClientConfig, IMockConfig, IAdminConfig } from '@forest-fire/types';
+import { ISdkFactory } from './ISdkFactory';
 
-export const FirestoreClient = {
-  create(): FC {
+/**
+ * **FirestoreClient** gives you access to the _Firestore_ database through Firebase's
+ * _Client_ SDK. This is intended to be used in frontend/browser based applications where
+ * the app can not be trusted to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `@firebase/app`
+ * - `@firebase/firestore`
+ * - optionally, `@firebase/auth` if you plan on using Firebase's auth system
+ *
+ * You should also include the following **devDependencies**:
+ * - `@firebase/app-types`
+ * - `@firebase/firestore-types`
+ * - `@firebase/auth-types`
+ *
+ * You may also optionally install the `firemock` library if you want to use the mock database
+ * capabilities that `universal-fire` provides. */
+export const FirestoreClient: ISdkFactory<IFirestoreClient, IClientConfig> = {
+  create() {
     return new FC();
   },
 
-  async connect(config: IClientConfig | IMockConfig): Promise<FC> {
+  async connect(config) {
     return FC.connect(config);
   },
 };
-export const RealTimeClient = {
-  create(): RTC {
+
+/**
+ * **RealTimeClient** gives you access to the _Real-Time_ database through Firebase's
+ * Client SDK. This is intended to be used in frontend/browser based applications where
+ * the app can not be trusted to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `@firebase/app`
+ * - `@firebase/database`
+ * - optionally, `@firebase/auth` if you plan on using Firebase's auth system
+ *
+ * You should also include the following **devDependencies**:
+ * - `@firebase/app-types`
+ * - `@firebase/database-types`
+ * - `@firebase/auth-types`
+ */
+export const RealTimeClient: ISdkFactory<IRealTimeClient, IClientConfig> = {
+  create() {
     return new RTC();
   },
 
-  async connect(config: IClientConfig | IMockConfig): Promise<RTC> {
+  async connect(config) {
     return RTC.connect(config);
   },
 };
 
-export const FirestoreAdmin = {
-  create(): IFirestoreAdmin {
+/**
+ * **FirestoreAdmin** gives you access to the _Firestore_ database through Firebase's
+ * Admin SDK. This is intended to be used in backend environments where the application
+ * is able to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `firebase-admin`
+ *
+ * You may also optionally install the `firemock` library if you want to use the mock database
+ * capabilities that `universal-fire` provides.
+ */
+export const FirestoreAdmin: ISdkFactory<IFirestoreAdmin, IAdminConfig> = {
+  create() {
     throw new Error(
       'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
     );
   },
 
-  async connect(...args: any[]): Promise<IFirestoreAdmin> {
+  async connect(config) {
     throw new Error(
       'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
     );
   },
 };
-export const RealTimeAdmin = {
-  create(): IRealTimeAdmin {
+
+/**
+ * **RealTimeAdmin** gives you access to the _Real Time_ database through Firebase's
+ * Admin SDK. This is intended to be used in backend environments where the application
+ * is able to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `firebase-admin`
+ *
+ * You may also optionally install the `firemock` library if you want to use the mock database
+ * capabilities that `universal-fire` provides.
+ */
+export const RealTimeAdmin: ISdkFactory<IRealTimeAdmin, IAdminConfig> = {
+  create() {
     throw new Error(
       'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
     );
   },
 
-  async connect(...args: any[]): Promise<IRealTimeAdmin> {
+  async connect(config) {
     throw new Error(
       'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
     );
--- packages/universal-fire/src/index.node.ts
+++ packages/universal-fire/src/index.node.ts
@@ -1,59 +1,118 @@
 export * from './proxy-symbols';
-
-export type { IAdminAuth as IAuth } from '@forest-fire/types';
-export type { IAdminConfig as IConfig } from '@forest-fire/types';
-
-export type { RealTimeAdmin as IRealTime } from '@forest-fire/real-time-admin';
-export type { FirestoreAdmin as IFirestore } from '@forest-fire/firestore-admin';
+export * from './sdk-types';
+import {
+  IFirestoreAdmin,
+  IFirestoreClient,
+  IRealTimeAdmin,
+  IRealTimeClient,
+} from './sdk-types';
 
 import { FirestoreAdmin as FSA } from '@forest-fire/firestore-admin';
 import { RealTimeAdmin as RTA } from '@forest-fire/real-time-admin';
-import { FirestoreClient as FC } from '@forest-fire/firestore-client';
-import { RealTimeClient as RTC } from '@forest-fire/real-time-client';
 import { IClientConfig, IMockConfig, IAdminConfig } from '@forest-fire/types';
+import { ISdkFactory } from './ISdkFactory';
 
-export const FirestoreClient = {
-  create(): FC {
+/**
+ * **FirestoreClient** gives you access to the _Firestore_ database through Firebase's
+ * _Client_ SDK. This is intended to be used in frontend/browser based applications where
+ * the app can not be trusted to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `@firebase/app`
+ * - `@firebase/firestore`
+ * - optionally, `@firebase/auth` if you plan on using Firebase's auth system
+ *
+ * You should also include the following **devDependencies**:
+ * - `@firebase/app-types`
+ * - `@firebase/firestore-types`
+ * - `@firebase/auth-types`
+ *
+ * You may also optionally install the `firemock` library if you want to use the mock database
+ * capabilities that `universal-fire` provides.
+ */
+export const FirestoreClient: ISdkFactory<IFirestoreClient, IClientConfig> = {
+  create() {
     throw new Error(
       'You are using the node entry point for universal-fire; use FirestoreAdmin instead.'
     );
   },
 
-  async connect(config: IClientConfig | IMockConfig): Promise<FC> {
+  async connect(config) {
     throw new Error(
       'You are using the node entry point for universal-fire; use FirestoreAdmin instead.'
     );
   },
 };
-export const RealTimeClient = {
-  create(): RTC {
+/**
+ * **RealTimeClient** gives you access to the _Real-Time_ database through Firebase's
+ * Client SDK. This is intended to be used in frontend/browser based applications where
+ * the app can not be trusted to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `@firebase/app`
+ * - `@firebase/database`
+ * - optionally, `@firebase/auth` if you plan on using Firebase's auth system
+ *
+ * You should also include the following **devDependencies**:
+ * - `@firebase/app-types`
+ * - `@firebase/database-types`
+ * - `@firebase/auth-types`
+ *
+ * You may also optionally install the `firemock` library if you want to use the mock database
+ * capabilities that `universal-fire` provides.
+ */
+export const RealTimeClient: ISdkFactory<IRealTimeClient, IClientConfig> = {
+  create() {
     throw new Error(
       'You are using the node entry point for universal-fire; use RealTimeAdmin instead.'
     );
   },
 
-  async connect(config: IClientConfig | IMockConfig): Promise<RTC> {
+  async connect(config: IClientConfig | IMockConfig) {
     throw new Error(
       'You are using the node entry point for universal-fire; use RealTimeAdmin instead.'
     );
   },
 };
 
-export const FirestoreAdmin = {
-  create(): FSA {
+/**
+ * **FirestoreAdmin** gives you access to the _Firestore_ database through Firebase's
+ * Admin SDK. This is intended to be used in backend environments where the application
+ * is able to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `firebase-admin`
+ *
+ * You may also optionally install the `firemock` library if you want to use the mock database
+ * capabilities that `universal-fire` provides.
+ */
+export const FirestoreAdmin: ISdkFactory<IFirestoreAdmin, IAdminConfig> = {
+  create() {
     return new FSA();
   },
 
-  async connect(config: IAdminConfig | IMockConfig): Promise<FSA> {
+  async connect(config: IAdminConfig | IMockConfig) {
     return FSA.connect(config);
   },
 };
-export const RealTimeAdmin = {
-  create(): RTA {
+
+/**
+ * **RealTimeAdmin** gives you access to the _Real Time_ database through Firebase's
+ * Admin SDK. This is intended to be used in backend environments where the application
+ * is able to hold secrets.
+ *
+ * To use this SDK, you should install the following **dependencies** in your app:
+ * - `firebase-admin`
+ *
+ * You may also optionally install the `firemock` library if you want to use the mock database
+ * capabilities that `universal-fire` provides.
+ */
+export const RealTimeAdmin: ISdkFactory<IRealTimeAdmin, IAdminConfig> = {
+  create() {
     return new RTA();
   },
 
-  async connect(config: IAdminConfig | IMockConfig): Promise<RTA> {
+  async connect(config: IAdminConfig | IMockConfig) {
     return RTA.connect(config);
   },
 };
