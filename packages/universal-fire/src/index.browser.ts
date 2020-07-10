da61af0e9ceb99fd350bda4035fbb2b42917380d
Ken Snyder <ken@ken.net>
2020-07-09 17:31:46 -0700

    both entry points export all 4 SDK's

--- packages/universal-fire/src/index.browser.ts
+++ packages/universal-fire/src/index.browser.ts
@@ -1,10 +1,56 @@
 export * from './proxy-symbols';
 
-export type { IClientAuth as IAuth } from '@forest-fire/types';
-export type { IClientConfig as IConfig } from '@forest-fire/types';
+export type { RealTimeClient as IRealTimeClient } from '@forest-fire/real-time-client';
+export type { FirestoreClient as IFirestoreClient } from '@forest-fire/firestore-client';
 
-export type { RealTimeClient as IRealTime } from '@forest-fire/real-time-client';
-export type { FirestoreClient as IFirestore } from '@forest-fire/firestore-client';
+import type { RealTimeAdmin as IRealTimeAdmin } from '@forest-fire/real-time-admin';
+import type { FirestoreAdmin as IFirestoreAdmin } from '@forest-fire/firestore-admin';
+import { FirestoreClient as FC } from '@forest-fire/firestore-client';
+import { RealTimeClient as RTC } from '@forest-fire/real-time-client';
+import { IClientConfig, IMockConfig } from '@forest-fire/types';
 
-export { FirestoreClient as Firestore } from '@forest-fire/firestore-client';
-export { RealTimeClient as RealTime } from '@forest-fire/real-time-client';
+export const FirestoreClient = {
+  create(): FC {
+    return new FC();
+  },
+
+  async connect(config: IClientConfig | IMockConfig): Promise<FC> {
+    return FC.connect(config);
+  },
+};
+export const RealTimeClient = {
+  create(): RTC {
+    return new RTC();
+  },
+
+  async connect(config: IClientConfig | IMockConfig): Promise<RTC> {
+    return RTC.connect(config);
+  },
+};
+
+export const FirestoreAdmin = {
+  create(): IFirestoreAdmin {
+    throw new Error(
+      'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
+    );
+  },
+
+  async connect(...args: any[]): Promise<IFirestoreAdmin> {
+    throw new Error(
+      'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
+    );
+  },
+};
+export const RealTimeAdmin = {
+  create(): IRealTimeAdmin {
+    throw new Error(
+      'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
+    );
+  },
+
+  async connect(...args: any[]): Promise<IRealTimeAdmin> {
+    throw new Error(
+      'You are using the client/browser entry point for universal-fire; use RealTimeClient instead.'
+    );
+  },
+};
--- packages/universal-fire/src/index.node.ts
+++ packages/universal-fire/src/index.node.ts
@@ -6,5 +6,54 @@ export type { IAdminConfig as IConfig } from '@forest-fire/types';
 export type { RealTimeAdmin as IRealTime } from '@forest-fire/real-time-admin';
 export type { FirestoreAdmin as IFirestore } from '@forest-fire/firestore-admin';
 
-export { FirestoreAdmin as Firestore } from '@forest-fire/firestore-admin';
-export { RealTimeAdmin as RealTime } from '@forest-fire/real-time-admin';
+import { FirestoreAdmin as FSA } from '@forest-fire/firestore-admin';
+import { RealTimeAdmin as RTA } from '@forest-fire/real-time-admin';
+import { FirestoreClient as FC } from '@forest-fire/firestore-client';
+import { RealTimeClient as RTC } from '@forest-fire/real-time-client';
+import { IClientConfig, IMockConfig, IAdminConfig } from '@forest-fire/types';
+
+export const FirestoreClient = {
+  create(): FC {
+    throw new Error(
+      'You are using the node entry point for universal-fire; use FirestoreAdmin instead.'
+    );
+  },
+
+  async connect(config: IClientConfig | IMockConfig): Promise<FC> {
+    throw new Error(
+      'You are using the node entry point for universal-fire; use FirestoreAdmin instead.'
+    );
+  },
+};
+export const RealTimeClient = {
+  create(): RTC {
+    throw new Error(
+      'You are using the node entry point for universal-fire; use RealTimeAdmin instead.'
+    );
+  },
+
+  async connect(config: IClientConfig | IMockConfig): Promise<RTC> {
+    throw new Error(
+      'You are using the node entry point for universal-fire; use RealTimeAdmin instead.'
+    );
+  },
+};
+
+export const FirestoreAdmin = {
+  create(): FSA {
+    return new FSA();
+  },
+
+  async connect(config: IAdminConfig | IMockConfig): Promise<FSA> {
+    return FSA.connect(config);
+  },
+};
+export const RealTimeAdmin = {
+  create(): RTA {
+    return new RTA();
+  },
+
+  async connect(config: IAdminConfig | IMockConfig): Promise<RTA> {
+    return RTA.connect(config);
+  },
+};
