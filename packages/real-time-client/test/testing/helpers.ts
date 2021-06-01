7ec9aac938ab64df17dfbffb74c5f58986b4328d
Robinson Villegas <robinsondotnet@hotmail.com>
2021-06-01 17:10:40 +0200

    chore: fix real-time-client deps

--- packages/real-time-client/test/testing/helpers.ts
+++ packages/real-time-client/test/testing/helpers.ts
@@ -48,7 +48,7 @@ export function setupEnv() {
   }
 
   const current = process.env;
-  const yamlConfig = yaml.safeLoad(fs.readFileSync('./env.yml', 'utf8')) as IDictionary;
+  const yamlConfig = yaml.load(fs.readFileSync('./env.yml', 'utf8')) as IDictionary;
   const combined = {
     ...yamlConfig[process.env.AWS_STAGE],
     ...process.env,
