16fdf6a00fcb75d724593b012791f452332d93e6
Robinson Villegas <robinsondotnet@hotmail.com>
2021-08-03 15:25:22 +0200

    chore(firemodel): get dbIndex from class reflector meta prop

--- packages/firemodel/src/decorators/model-meta/relns/getDbIndexes.ts
+++ packages/firemodel/src/decorators/model-meta/relns/getDbIndexes.ts
@@ -1,18 +1,17 @@
 import { ClassReflection } from "~/decorators/utils";
 import { IModelIndexMeta } from "~/types/property-meta/IModelIndexMeta";
-import { getRelationships } from "./getRelationships";
 
 /**
  * Gets all the db indexes for a given model
  */
 export function getDbIndexes(klass: ClassReflection): IModelIndexMeta[] {
-  return getRelationships(klass).filter(r => r.isIndex).map(r => ({
-    isIndex: r.isIndex ? true : false,
-    isUniqueIndex: r.isUniqueIndex ? true : false,
-    isMultiEntryIndex: r.isMultiEntryIndex ? true : false,
-    property: r.property,
-    desc: r.desc
-  }));
-
-
+  return Object.entries(klass.meta)
+    .filter(([k, r]) => r.isIndex)
+    .map(([k, r]) => ({
+      isIndex: r.isIndex ? true : false,
+      isUniqueIndex: r.isUniqueIndex ? true : false,
+      isMultiEntryIndex: r.isMultiEntryIndex ? true : false,
+      property: r.property,
+      desc: r.desc,
+    }));
 }
